94d86dd77388f9228193fbad5750e7bc2a095d89
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2023-03-29 14:01:53 +0300

    color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const body = document.querySelector('body');
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+btnStart.addEventListener('click', changeBackgroundColor);
+btnStop.addEventListener('click', stopChangeColor);
+
+let interval = null;
+
+function changeBackgroundColor() {
+    interval = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    btnStart.disabled = true;
+};
+
+function stopChangeColor() {
+    clearInterval(interval);
+    btnStart.disabled = false;
+ }
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`
+};
+
+
 
