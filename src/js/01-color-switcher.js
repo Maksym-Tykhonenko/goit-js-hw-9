8c774aa7afef88dcc9ac6ca7a1fa723ad8988b86
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2023-04-02 17:26:03 +0300

    horses raice

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -23,5 +23,3 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`
 };
 
-
-
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,78 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+//function createPromise(position, delay) {
+//  const shouldResolve = Math.random() > 0.3;
+//  if (shouldResolve) {
+//    // Fulfill
+//  } else {
+//    // Reject
+//  }
+//}
+const horses = [
+    'СЕКРИТАРИАТ',
+    'ЭКЛИПС',
+    'ВЕСТ АВСТРАЛИАН',
+    'ФЛАИНГ ФОКС',
+    'СЕБИСКЕТ',
+];
+
+let raceCounter = 0;
+
+const refs = {
+    startBtn: document.querySelector('.js-rase-btn'),
+    winner: document.querySelector('.winner'),
+    progress: document.querySelector('.progress'),
+    tableBody: document.querySelector('.js-result-table > tbody'),
+};
+
+refs.startBtn.addEventListener('click', onStart);
+
+function onStart() {
+    raceCounter += 1;
+    const promises = horses.map(horse => run(horse));
+
+    updWinnerField('');
+    updProgressField('Заезд начался, ставки не принимаются');
+    determinateWinner(promises);
+    waitForAll(promises);
+};
+
+
+function determinateWinner(horsesP) {
+    Promise.race(horsesP)
+        .then(({ horse, time }) => {
+            updWinnerField(`Победил ${horse}, финишировал за ${time}`)
+            updResultTable({ horse, time, raceCounter })
+        });
+};
+
+function waitForAll(horsesP) {
+    Promise.all(horsesP).then(() =>
+        updProgressField('Заезд окончен, принимаются ставки.')
+    );
+};
+
+function updWinnerField(message) {
+    refs.winner.textContent = message;
+};
+
+function updProgressField(message) {
+    refs.progress.textContent = message;
+};
+
+function updResultTable({ horse, time, raceCounter }) {
+    const tr = `<tr><td>${raceCounter}</td><td>${horse}</td><td>${time}</td></tr>`;
+    refs.tableBody.insertAdjacentHTML('beforeend', tr);
+};
+
+function getRandomTime(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function run(horse) {
+    return new Promise((resolve) => {
+        const time = getRandomTime(2000, 3500);
+
+        setTimeout(() => {
+            resolve({ horse, time });
+        }, time);
+    })
+};
